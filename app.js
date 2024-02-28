e895ff2c4351a1ce602a4895b627ab314251b718
lorem ipsum dolor <118758397+loremipsumx999@users.noreply.github.com>
2024-02-28 22:01:52 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let skala = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function nyalas(){
+    let i = 0;
+    let valasz = prompt("Mennyire? 1-10");
+    if (valasz != skala[i]){
+        alert("1-10-ig számmal bro")
+    }
+    else if (valasz >= 8){
+        alert("Az má komoly 😩")
+    }
+    alert("JÁKOB WALK");
+}
